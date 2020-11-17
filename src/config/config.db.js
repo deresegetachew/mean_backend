60786e395e5b8d907c5435fa4c3cb00b76c42170
derese getachew <deresegetachew@gmail.com>
2020-11-17 14:39:22 +0300

    mongoose connect

--- src/config/config.db.js
+++ src/config/config.db.js
@@ -1,35 +1,18 @@
 const mongoose = require('mongoose');
 
+
 const connectToDb = async () => {
-    let db = await mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true }, (err) => {
-        console.log(err);
-    });
 
-
-    if (db) {
-        db.connection.on('connecting', () => {
-
-        });
-
-        db.connection.on('error', () => {
-
-        });
-
-        db.connection.on('connected', () => {
-            console.log('connected to database');
-        })
-        db.connection.on('open', () => {
-            console.log("databse connection opened");
-        })
-        db.connection.on('reconnected', () => {
-            console.log("reconnecting to database");
-        })
-        db.connection.on('disconnected', () => {
-
-        });
-
-        return db.connection.db;
+    try {
+        mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
     }
+    catch (e) {
+        console.log("???", e);
+    }
+
+
+
+
 
 }
 
