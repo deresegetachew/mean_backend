b2a2bfdd66d93da697c8bfda27db22a384a116de
derese getachew <deresegetachew@gmail.com>
2020-11-17 15:03:55 +0300

    mongoose connect to db

--- src/config/config.db.js
+++ src/config/config.db.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose');
 const connectToDb = async () => {
 
     try {
-        await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect('mongodb://localhost:27018/meandb', { useNewUrlParser: true }, (err) => {
+        });
+
+        // await mongoose.connect('mongodb://localhost/meandb', { useNewUrlParser: true }, (err) => {
+        // });
 
         mongoose.connection.once('opened', () => { console.log("connection opened") });
-        mongoose.connection.on('connected', () => { console.log('connected to mongodb') })
+        mongoose.connection.on('connected', () => { console.log('connected to mongodb') });
+        mongoose.connection.on('error', (err) => { console.log("on error", err) });
     }
     catch (e) {
         console.log("???", e);
