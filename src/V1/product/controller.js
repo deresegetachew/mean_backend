d7666028b4c68074d10baa2a0113c5c99b9cdfbf
derese getachew <deresegetachew@gmail.com>
2020-11-18 10:59:13 +0300

    get and getById using router parameters

--- src/V1/product/controller.js
+++ src/V1/product/controller.js
@@ -1,8 +1,29 @@
+const mongoose = require('mongoose');
 const { productModel } = require('./model');
 
+//query parameter currentPage , perPage 
+//skip, limit 
+
 getProducts = async (req, res, next) => {
-    let allProducts = await productModel.find()
+    console.log(req.query);
+    let allProducts = await productModel.find({})
     res.send(allProducts);
+}
+
+
+getProductByName = async (req, res, next) => {
+    let { name } = req.params;
+
+    let products = await productModel.find({ name }).exec();
+
+    res.status(200).send(products);
+}
+
+getProductById = async (req, res, next) => {
+    const { id } = req.params;
+
+    let prod = await productModel.find({ _id: mongoose.Types.ObjectId(id) }).exec();
+    res.status(200).send(prod);
 }
 
 createProducts = async (req, res, next) => {
@@ -15,4 +36,4 @@ createProducts = async (req, res, next) => {
 }
 
 
-module.exports = { getProducts, createProducts };
+module.exports = { getProducts, createProducts, getProductByName, getProductById };
--- src/V1/product/route.js
+++ src/V1/product/route.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const { reset } = require('nodemon');
+const { getProductByName, getProductById } = require('./controller');
 const productController = require('./controller');
 
 
@@ -7,6 +8,8 @@ var router = express.Router({ mergeParams: true });
 
 
 router.get('/', (req, res, next) => { productController.getProducts(req, res, next) });
+router.get('/:name', getProductByName)
+router.get('/byid/:id', getProductById)
 router.post('/', productController.createProducts)
 
 
